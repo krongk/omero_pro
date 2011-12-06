f37b2f5e256fd69f9fbe112a5dd30c07397acffc
krongk <kenrome@gmail.com>
2011-12-07 00:32:07 +0800

    add pages

--- rails_on_web/app/helpers/application_helper.rb
+++ rails_on_web/app/helpers/application_helper.rb
@@ -9,4 +9,15 @@ module ApplicationHelper
   def meta_description(meta_description)
   	content_for(:meta_description){ meta_description}
   end
+  #input: nav_bar [['首页', '/'], ['关于', '/about']]
+  def nav_bar(bar_arr)
+  	strs = []
+	bar_arr.each do |nav|
+	  strs << link_to(nav[0], nav[1], :class=> 'nav_bar_link')
+	end
+	str = strs.join('/')
+  	content_for(:nav_bar){
+  	  str.html_safe
+  	}
+  end
 end
