f0712f0a3f377fb4a055ab558e0d29e4b54ba5cf
krongk <kenrome@gmail.com>
2013-04-12 10:32:15 +0800

    update

--- rails_on_web/config/routes.rb
+++ rails_on_web/config/routes.rb
@@ -107,7 +107,7 @@ RailsOnWeb::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'home#index'
+  root :to => 'pages#index'
   #root :to => 'pages#show'
 
   # See how all your routes lay out with "rake routes"
